22898a63e92b332e1d669667e082e2502f911d2a
murichGhostdrive <109663630+murichGhostdrive@users.noreply.github.com>
2023-07-13 20:15:57 +0800

    Create prop.gd

---
+++ prop.gd
@@ -0,0 +1,185 @@
+# Open Grant Proposal: `Ghostdrive`
+
+
+**Project Name:**
+Ghostdrive - Smart Contracts and Tokenization (FVM)
+
+
+**Proposal Category:** `Applications`
+
+
+**Individual or Entity Name:** `Ghostdrive Global`
+
+
+**Proposer:** `murichGhostdrive` :)
+
+
+**(Optional) Technical Sponsor:** `Paul Wagner`
+
+
+**Do you agree to open source all work you do on behalf of this RFP under the MIT/Apache-2 dual-license?:**
+`Yes`
+
+
+# Project Summary
+
+
+Ghostdrive leverages the benefits of file encryption, decentralized storage, and asset tokenization using a multi-platform application. By harnessing IPFS and Filecoin technologies, it provides an innovative and secure way of storing, sharing, and enabling data exchange markets. Users are in control of their data because of client side encryption and the use of decentralized storage networks.
+
+
+Moreover, Ghostdrive's capabilities allow for seamless interaction with its services, irrespective of the device or operating system in use. It is designed with both an intuitive user interface and command line tools in order to simplify the process of uploading, accessing, and sharing data. Additionally, it incorporates smart contracts to tokenize data access.
+
+
+## Impact
+
+
+The Ghostdrive project addresses several significant pain points that are inherent in traditional data storage methods:
+
+
+- Privacy Concerns: Traditional data storage services do not support client side encryption, posing serious privacy concerns. They could potentially monitor, share, or misuse user data. Ghostdrive, by providing client side encryption tools, allows users to have full control over their data.
+
+
+- Reliability of decentralized networks: Using centralized service brings up the problem of point of failure. Ghostdrive mitigates this risk by allowing unlimited replication using IPFS and Filecoin technologies.
+
+
+<!--- What are the benefits to getting this right? What are the risks of not getting this right?
+- What impact will this project have in a specific vertical, market, or ecosystem? What does success look like? -->
+
+
+## Outcomes
+
+
+<!-- Please describe in detail what your final outcomes & deliverable(s) for this project will be. Include a specification of the project and what functionality the software will deliver when it is finished.-->
+The Ghostdrive project is a cross-platform application. Here are the expected final outcomes and deliverables:
+
+
+- Fully Functional Application: The final product will be a user-friendly application that can be utilized across various platforms such as Web, Windows, macOS, Linux, Android, and iOS. The application will allow users to easily store, access, and share data while protecting their privacy thanks to client side encryption.
+- Decentralized Storage Mechanism: Our service stores users files in IPFS and Filecoin network allows users to replicate their data the way they need.
+- Scalability: The application will be scalable, allowing users to expand storage as required. For corporate users, this will allow for growth without the need to invest in costly additional infrastructure.
+- Privacy and Security: Users will maintain control over their data.
+- Cross-Platform Compatibility: Users will be able to access their stored data seamlessly, regardless of the platform they use. The software will be designed to operate smoothly on various operating systems.
+- Collaboration Tools: The software will also provide tools for secure data sharing and collaboration. Users will be able to share data with others safely, making it a valuable tool for corporate collaboration.
+- Data exchange market: Access tokenization enables Data Exchange Market on blockchain
+
+
+
+
+<!-- Please describe how your team is measuring the success of your project and define the metrics being used.-->
+Success Metrics:
+
+
+- corporate client onboarding and adoption rate
+- retail client onboarding and adoption rate
+- components performance metrics
+
+
+## Adoption, Reach, and Growth Strategies
+
+Our target audience is two-fold: individual users seeking secure, private, and flexible data storage solutions, and businesses looking for cost-effective, scalable, and reliable data storage and sharing platforms. This audience is vast as data storage is a fundamental need in our digital age. We are currently engaging with them through direct outreach, digital marketing strategies, and partnerships with relevant organizations and influencers in the tech industry.
+
+Our onboarding strategies are as follows:
+
+First Users: We plan to bring onboard tech enthusiasts from our personal and professional networks who understand the value and potential of decentralized storage. We will provide them with personalized product demonstrations and support to ensure they understand how to get the most out of Ghostdrive and collect their feedback.
+
+Second Wave: To expand our reach, we plan to utilize our partnership with Coin Telegraph, a leading digital media, events, and information services company for the crypto asset and blockchain technology community. This collaboration will help us reach a broader audience. In addition, our dedicated marketing team will run targeted campaigns across various channels like social media, tech forums, and email newsletters. We  have influencer network prepared as well.
+
+## Development Roadmap
+
+
+### FVM Integration for Access Tokenization
+
+Functionality: The integration of the FVM (Filecoin Virtual Machine) for access tokenization provides the functionality of secure access control. It enables the creation of tokens which are used to access stored data, ensuring that only authorized users can access specific data.
+Team Members: Business analyst, UI designer, Frontend developer, Backend developer, Blockchain developer, Quality Assurance Engineer
+Funding: $25,000 was allocated for this milestone. This covered the costs of manpower, additional hardware and software requirements, and testing.
+Timeline: This milestone will be completed on Aug 10, 2023.
+
+### Multisig file management
+
+Functionality: This milestone will enable multiple-signature file management in Ghostdrive using FVM and other blockchains. This feature will provide enhanced security for file management, requiring multiple parties to authorize file sharing or deletion.
+Team Members: Business analyst, UI designer, Frontend developer, Backend developer, Blockchain developer, Quality Assurance Engineer
+Funding: This phase requires $40,000. This will cover salaries, additional security measures, potential audits, and any necessary hardware or software.
+Timeline: This milestone is expected to take 1 month. Starting from Aug 5, 2023, the expected completion date is September 5, 2023.
+
+
+### Data access IDO
+Functionality: This milestone will enable the Initial Decentralized Offering (IDO) for data access tokens on Ghostdrive. This will provide a mechanism for users to trade access to files and data in a secure, decentralized manner.
+Team Members: Business analyst, UI designer, Frontend developer, Backend developer, Blockchain developer, Quality Assurance Engineer
+Funding: This phase requires $30,000. This funding will cover salaries, marketing and promotional activities, and other related expenses.
+Timeline: This milestone is expected to take 1 month. Starting from September 6, 2023, the expected completion date is October 7, 2023.
+
+
+
+## Total Budget Requested
+
+
+| Milestone # | Description                                     | Deliverables                                 | Completion Date | Funding    |
+|-------------|-------------------------------------------------|----------------------------------------------|-----------------|------------|
+| 1           | FVM integration for access tokenization (MVP)   | FVM integration, Tokenized file access       | Aug 10, 2023    | $30,000   |
+| 2           | Multisig file management                        | Multisig security protocol for file management| September 5, 2023   | $40,000   |
+| 3           | Data access IDO                                 | Decentralized marketplace for data access    | October 7, 2023    | $30,000   |
+
+
+
+
+[![](https://mermaid.ink/img/pako:eNqFUEFqwzAQ_MqiUws22A6lrc4m0INIoBAo6LLYa0etJQVrfWhD_t5NVedWOqdhZpkd5qy62JPSasTAbAMIemTaxtkjA7wJSmPKts0eO54I9nN8p47Bu4kSx0Apu0lEFwOYVc_y9mDABaZxxh97iDNg11FKwPGDgvvK-p057O9BY11AUzWbsnos62rlT8JznFkmdsmNMMgb8BhwJE-B4Q9obArAgUm-3qKfy-ohx7XIuNZ5aXfwHzRubnHNb1xdSVkbVKE8yXCul0XP13ir-CjlrNJCexpQultlw0VOceH4-hk6pXleqFDL6bp861B28koPOCW6fAPIQ3gx?type=png)](https://mermaid.live/edit#pako:eNqFUEFqwzAQ_MqiUws22A6lrc4m0INIoBAo6LLYa0etJQVrfWhD_t5NVedWOqdhZpkd5qy62JPSasTAbAMIemTaxtkjA7wJSmPKts0eO54I9nN8p47Bu4kSx0Apu0lEFwOYVc_y9mDABaZxxh97iDNg11FKwPGDgvvK-p057O9BY11AUzWbsnos62rlT8JznFkmdsmNMMgb8BhwJE-B4Q9obArAgUm-3qKfy-ohx7XIuNZ5aXfwHzRubnHNb1xdSVkbVKE8yXCul0XP13ir-CjlrNJCexpQultlw0VOceH4-hk6pXleqFDL6bp861B28koPOCW6fAPIQ3gx)
+
+
+
+
+## Maintenance and Upgrade Plans
+
+
+- Maintenance and Bug Fixes: Post-launch, the team will ensure that the system runs smoothly and that any bugs that arise are promptly addressed. We plan to have dedicated developers on board for regular system checks, maintenance, and bug fixes.
+- Continuous Development: We plan to roll out updates with improved functionalities and features based on feedback from users and our own assessments. The aim is to continue enhancing user experience and application performance.
+- Security Upgrades: In a constantly changing digital landscape, threats and vulnerabilities evolve as well. Therefore, it's crucial to maintain an up-to-date security protocol. Our team will be dedicated to incorporating the latest security measures to protect user data and privacy.
+- Partnerships and Integrations: To enhance the Ghostdrive's capabilities and reach, we'll explore partnerships with other tech firms and potential integrations with other services. For example, we may integrate with popular productivity tools or explore collaborations that will allow us to offer more to our users.
+- Sustainability: To ensure the project's financial sustainability, we'll continually refine our business model. This could involve introducing premium features, partnering with businesses, or exploring other revenue models while ensuring that we continue to offer robust free features for our users.
+- Feedback and Community Engagement: We'll maintain an active feedback loop with our user community and use their inputs to shape our development roadmap. This can include regular surveys, a community forum for discussions, and beta testing of new features with engaged users.
+
+
+# Team
+
+
+## Team Members
+
+
+Roman Tykhovsky (CEO) https://www.linkedin.com/in/romansky/
+
+
+Andrii Murashkin (CTO) https://www.linkedin.com/in/andrey-murashkin-32914920/
+
+
+Anna Vazhlivtseva (Back-End Lead) https://www.linkedin.com/in/anna-vazhlivtseva-1a5b9339/
+
+
+Vadym Vitenko (Blockchain Lead) https://www.linkedin.com/in/vadym-vitenko-600824164/
+
+
+Andrii Svirskyi (Front-End Lead )https://www.linkedin.com/in/andrii-svirskyi-6a1458151/
+
+
+
+
+## Relevant Experience
+
+
+Roman (Nebo) Tykhovsky: An entrepreneurial powerhouse, our leader boasts a rich background as a CTO in an array of successful startups, skillfully blending technical prowess with business acumen.
+
+Anna Vazhlivtseva: An accomplished backend developer, our team member leverages ten years of diverse coding experience, serving as a vital pillar in our technical operations.
+
+Vadym Vitenko: Experienced Javascript Developer with a demonstrated history of working in the program development industry. Skilled in typescript, npm, SASS, ReactJS. Strong engineering professional with a Bachelor of Engineering - BE focused in Computer Software Engineering from Ternopil Academy of National Economy.
+
+Andrii Svirskyi: As a seasoned frontend developer, Andrii spent 8 years mastering React, Angular, and Vue, and leading teams over the last three. Concurrently, I've deepened my knowledge in blockchain technology, becoming proficient in Solana, Binance, Polygon, and Ethereum.
+
+Andrii Murashkin: Our CTO, brings 15 years of software development and architectural expertise to the table. A pioneer in the crypto world, he run oclcalc.com and focalway.com bitcoin related projects back in 2011.
+
+
+
+# Additional Information
+We learned about the Open Grants Program thanks to Paul Wagner.
+
+
+Please contact us using roman@ghostdrive.com
+
+
+
